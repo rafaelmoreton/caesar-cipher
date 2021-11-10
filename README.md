96fb8e48197eb3bef9ea920593846617bd919540
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2021-11-10 11:00:22 -0300

    Wrap from 'z' to 'a' logic implemented. Implemented negative shift factors with wrap from 'a' to 'z' capability. Added conditionals to make the cipher case sensitive. Created README.

---
+++ README.md
@@ -0,0 +1,2 @@
+Caesar cipher is a method that takes a string - the message - as the first argument and a integer - the shift factor - as the second.
+It returns a string presenting the original message ciphered (case sensitive) according to the shift factor. The original string's symbols and punctuation remain unchanged.
--- caesar cipher.rb
+++ caesar cipher.rb
@@ -1,11 +1,31 @@
 def caesar_cipher(string, shift_factor)
+
     ciphed_array = []
+    
     string.split("").each do |letter|
         letter_key = letter.ord
-        if letter_key > 64
+        if letter_key >= 65 && letter_key <= 90
             letter_key += shift_factor
-            letter_key = letter_key.chr
-            ciphed_array << letter_key
+            while letter_key > 90
+                letter_key -= 26
+            end
+            while letter_key < 65
+                letter_key += 26
+            end
+            ciphed_letter = letter_key.chr
+            ciphed_array << ciphed_letter
+
+        elsif letter_key >= 97 && letter_key <= 122
+            letter_key += shift_factor
+            while letter_key > 122
+                letter_key -= 26
+            end
+            while letter_key < 97
+                letter_key += 26
+            end
+            ciphed_letter = letter_key.chr
+            ciphed_array << ciphed_letter
+
         else ciphed_array << letter
         end
     end
